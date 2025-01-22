3e887247307880c8e6635e28e16a35c92f9e288b
walimuhammadmalik <walimalik111@gmail.com>
2025-01-22 15:55:02 +0500

    Revert "add Redis integration and blacklist token middleware"
    This reverts commit 21653a0b1214590a69265e7835815733a83fc487.

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { authMiddleware } = require('../middlewares/auth.middleware');
-const checkBlacklist = require('./auth.route');
 const {
   getUsers,
   getUserById,
@@ -10,7 +9,7 @@ const {
 
 const router = express.Router();
 
-router.get('/', checkBlacklist.checkBlacklist, authMiddleware, getUsers);
+router.get('/', authMiddleware, getUsers);
 router.get('/:id', authMiddleware, getUserById);
 router.patch('/:id', authMiddleware, updateUserById);
 router.delete('/:id', authMiddleware, deleteUserById);
